5a27e704687988891b4da5abf6484b570fc77b73
ltlaitoff <ltlaitoff@gmail.com>
2023-10-14 22:06:12 +0300

    feat(LB 3 ADS): Create task 1

--- ads/lb3/src/main.ts
+++ ads/lb3/src/main.ts
@@ -1,1 +1,205 @@
-console.log('Hello world!')
+import * as readline from 'node:readline'
+import * as nodeProcess from 'node:process'
+import * as chalk from 'chalk'
+
+const { stdin: input, stdout: output } = nodeProcess
+
+import { BringWater } from './water'
+
+const bringWater = new BringWater(0, [])
+
+/*
+	Menu 1:
+	(0) - Individual task
+	(1) - File task
+
+	Menu individual:
+	(1) - Set data
+	(2) - Change barrelСapacity
+	(3) - Change buckets
+	(4) - Show result
+	(0) - Go back
+
+	File menu:
+	(0) - Go back
+
+*/
+
+const rl = readline.createInterface({ input, output })
+
+function menu() {
+	const menuString =
+		`\nChoice menu:\n` +
+		`(1) - Individual task\n` +
+		`(2) - File task\n` +
+		`(0) - Close program\n` +
+		`\nInput(default - 0): `
+
+	rl.question(menuString, answer => {
+		console.clear()
+		try {
+			const number = Number(answer)
+
+			switch (number) {
+				case 0: {
+					rl.close()
+					return
+				}
+
+				case 1: {
+					individualMenu()
+					break
+				}
+
+				case 2: {
+					fileMenu()
+					break
+				}
+
+				default: {
+					throw new Error('Not valid input')
+				}
+			}
+		} catch {
+			console.log(chalk.red('Not valid input! Try again'))
+		}
+
+		menu()
+	})
+}
+
+function individualSetBarrelCapacityInput(): Promise<void> {
+	return new Promise(resolve => {
+		const menuString = `\nInput new barrel capacity(current = ${bringWater.barrelСapacity}): `
+
+		rl.question(menuString, async answer => {
+			console.clear()
+
+			try {
+				const number = Number(answer)
+
+				if (number < 0) {
+					throw new Error('Not valid input')
+				}
+
+				bringWater.barrelСapacity = number
+				resolve()
+				return
+			} catch {
+				console.log(chalk.red('Not valid input! Try again'))
+			}
+
+			await individualSetBarrelCapacityInput()
+			resolve()
+		})
+	})
+}
+
+function individualSetBuckets(): Promise<void> {
+	return new Promise(resolve => {
+		const menuString = `\nInput new buckets sizes in line with "," as divider (current = [${bringWater.buckets.join(
+			','
+		)}]): `
+
+		rl.question(menuString, async answer => {
+			console.clear()
+
+			try {
+				const buckets = answer.split(',').map(Number)
+
+				buckets.forEach(bucket => {
+					if (isNaN(bucket)) throw new Error()
+				})
+
+				bringWater.buckets = structuredClone(buckets)
+				resolve()
+				return
+			} catch {
+				console.log(chalk.red('Not valid input! Try again'))
+			}
+
+			await individualSetBuckets()
+			resolve()
+		})
+	})
+}
+
+function individualMenu() {
+	const menuString =
+		`\nMenu individual:\n` +
+		`(1) - Set data\n` +
+		`(2) - Change barrelСapacity\n` +
+		`(3) - Change buckets\n` +
+		`(4) - Show result\n` +
+		`(0) - Go back\n` +
+		`\nInput(default - 0): `
+
+	rl.question(menuString, async answer => {
+		console.clear()
+
+		try {
+			const number = Number(answer)
+
+			switch (number) {
+				case 0: {
+					menu()
+					return
+				}
+
+				case 1: {
+					await individualSetBarrelCapacityInput()
+					await individualSetBuckets()
+					break
+				}
+
+				case 2: {
+					await individualSetBarrelCapacityInput()
+					break
+				}
+
+				case 3: {
+					await individualSetBuckets()
+					break
+				}
+
+				case 4: {
+					console.log(bringWater.result)
+					break
+				}
+
+				default:
+					throw new Error('Not valid input')
+			}
+		} catch {
+			console.log(chalk.red('Not valid input! Try again'))
+		}
+
+		individualMenu()
+	})
+}
+
+function fileMenu() {
+	const menuString =
+		`\nFile menu:\n` + `(0) - Close program\n` + `\nInput(default - 0): `
+
+	rl.question(menuString, answer => {
+		console.clear()
+
+		try {
+			const number = Number(answer)
+
+			if (number === 0) {
+				menu()
+				return
+			}
+
+			throw new Error('Not valid input')
+		} catch {
+			console.log(chalk.red('Not valid input! Try again'))
+		}
+
+		fileMenu()
+	})
+}
+
+menu()
---
+++ ads/lb3/src/water.ts
@@ -0,0 +1,53 @@
+import * as chalk from 'chalk'
+
+/*
+Варіант No 3.
+На дачі стоїть велика діжка, яка вміщує задану кількість рідини.
+Хазяїн використовує її для поливу рослин, але не маючи
+централізованого водопостачання, має принести воду з річки. У його
+розпорядженні є відра заданого обсягу. Визначити, яку мінімальну
+кількість відер води хазяїну потрібно принести з річки, щоб заповнити
+діжку. Вважати, що кожне відро може бути принесене тільки повністю
+заповненим, адже хазяїн не хоче носити зайвий вантаж. Результати
+виводити, демонструючи кількість відер кожного обсягу.
+*/
+
+class BringWater {
+	barrelСapacity = 0
+	buckets: number[] = []
+
+	constructor(barrelСapacity: number, buckets: number[]) {
+		this.barrelСapacity = barrelСapacity
+		this.buckets = buckets.sort((a, b) => a - b)
+	}
+
+	get result() {
+		let notFilledCapacity = this.barrelСapacity
+
+		const usedBuckets = {}
+
+		if (this.buckets.length === 0) {
+			return chalk.red('Buckets must be!')
+		}
+
+		while (notFilledCapacity > 0) {
+			for (let i = 0; i < this.buckets.length; i++) {
+				const bucket = this.buckets[i]
+
+				if (notFilledCapacity < bucket || i === this.buckets.length - 1) {
+					if (!(bucket in usedBuckets)) {
+						usedBuckets[bucket] = 0
+					}
+
+					usedBuckets[bucket]++
+					notFilledCapacity -= bucket
+					break
+				}
+			}
+		}
+
+		return usedBuckets
+	}
+}
+
+export { BringWater }
